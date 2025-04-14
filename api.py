593ad987959458f2005eeda4ccfc91ef677a27c7
Astra Clelia Bertelli <astraberte9@gmail.com>
2025-04-15 01:24:44 +0200

    fixed resume parsing by moving it outside the workflow, made the agent work better

--- api.py
+++ api.py
@@ -3,12 +3,16 @@ from llama_index.llms.groq import Groq
 from llama_index.core.agent.workflow import AgentWorkflow, FunctionAgent, ToolCall, ToolCallResult
 from utils import ChatHistory
 from fastapi import FastAPI
+from llama_cloud_services import LlamaExtract
 from fastapi.responses import ORJSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 import gradio as gr
 import requests as rq
+from os import environ as ENV
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class ApiInput(BaseModel):
     resume: str
@@ -22,14 +26,18 @@ app = FastAPI(default_response_class=ORJSONResponse)
 hist = ChatHistory()
 mcp_client = BasicMCPClient("http://localhost:8000/sse")
 mcp_tools = McpToolSpec(mcp_client)
+llm = Groq(model="qwen-qwq-32b", api_key=ENV["groq_api_key"])
+extractor = LlamaExtract(api_key=ENV["llamacloud_api_key"])
+extractor_agent = extractor.get_agent(id="99d18493-f573-47ba-a421-d3336260d712")
 
 @app.post("/chat")
 async def chat(inpt: ApiInput) -> ApiOutput:
     tools = await mcp_tools.to_tool_list_async()
     agent = FunctionAgent(
+        llm = llm,
         name = "ResumeMatcher",
         description="Useful to match resume with jobs scraped from the web",
-        system_prompt="You are the ResumeMatcher agent. Your task is to match a resume with jobs you can find from the web, evaluate the matches and return to the user a comprehensive summary of these matches, using the available tools. You should follow this workflow:\n1. Parse the resume and get details about potential job titles that would be interesting for the candidate, seniority level, skills and location, using the 'resume_parser' tool (providing the path to the resume uploaded by the user)\n2. Use the potential job titles to perform a web search of the jobs and extract the top 5 matches within the EU, using the 'job_searcher' tool (providing it a list of potential job titles, derived from step (1))\n3. With the information derived from step (1) and (2), pass the candidate profile (from step (1)) and the jobs (in the same JSON string format as you got them from step (2)) to the 'evaluate_job_match' tool.\n\n4. From the job matching evaluation that you got from step (3), create a final response that summarizes the jobs and reports their match with the candidate. Don't forget to mention the company offering the job, the link to the job posting and the job title.\n\nDo not stop unless you created a final response.",
+        system_prompt="You are the ResumeMatcher agent. Your task is to match a resume with jobs you can find from the web, evaluate the matches and return to the user a comprehensive summary of these matches, using the available tools. You should follow this workflow:\n1. Use the potential job titles contained in the resume data to perform a web search of the jobs and extract the top 5 matches within the EU, using the 'job_searcher' tool (providing it a list of potential job titles)\n2. With the information derived from step (1), pass the candidate profile (from the input resume data) and the jobs (in the same JSON string format as you got them from step (1)) to the 'evaluate_job_match' tool.\n\n3. From the job matching evaluation that you got from step (2), create a final response that summarizes the jobs and reports their match with the candidate. Don't forget to mention the company offering the job, the link to the job posting and the job title.\n\nDo not stop unless you created a final response.",
         tools = tools
     )
     workflow = AgentWorkflow(
@@ -49,6 +57,17 @@ async def chat(inpt: ApiInput) -> ApiOutput:
     response = str(response)
     return ApiOutput(response = response, process = process)
 
+def resume_parser(path_to_resume: str):
+    response = extractor_agent.extract(path_to_resume)
+    extracted_data = response.data
+    formatted_data = f"""
+    Potential Job Roles: {', '.join(extracted_data['potential_job_titles'])}
+    Seniority: {extracted_data['seniority']}
+    Skills: {', '.join(extracted_data['skills'])}
+    Based in: {extracted_data['based_in'] if extracted_data['based_in'] is not None else 'Information not available'}
+    Working location: {extracted_data['work_location'] if extracted_data['work_location'] is not None else 'Information not available'}
+    """
+    return formatted_data
 
 def add_message(history: list, message: dict):
     for x in message["files"]:
@@ -60,13 +79,15 @@ def add_message(history: list, message: dict):
 def bot(history: list):
     messages = history.copy()
     messages.reverse()
-    msgs = [msg for msg in messages if isinstance(msg["content"], dict)]
+    print(messages)
+    msgs = [msg for msg in messages if isinstance(msg["content"], tuple)]
     if len(msgs) == 0:
         history.append({"role": "assistant", "content": "There is no attached resume"})
         return history
     else:
-        resume_path = msgs[0]
-        response = rq.post("http://localhost:7500/chat", json=ApiInput(resume=resume_path).model_dump())
+        resume_path = msgs[0]["content"][0]
+        parsed_resume = resume_parser(resume_path)
+        response = rq.post("http://localhost:7500/chat", json=ApiInput(resume=parsed_resume).model_dump())
         if response.status_code == 200:
             res_json = response.json()
             agent_process = res_json["process"]
@@ -96,3 +117,5 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Resume Matcher") as demo:
     )
     bot_msg = chat_msg.then(bot, chatbot, chatbot, api_name="bot_response")
     bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
+
+app = gr.mount_gradio_app(app, demo, "/")
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from mcp.server.fastmcp import FastMCP
 import requests as rq
-from llama_cloud_services import LlamaExtract
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from os import environ as ENV
@@ -8,6 +7,7 @@ from typing import List
 from llama_index.core.llms import ChatMessage
 import json
 from llama_index.llms.groq import Groq
+import argparse
 
 load_dotenv()
 
@@ -15,24 +15,11 @@ class JobMatchEvaluation(BaseModel):
     match_score: int = Field(description="An evaluation, between 0 and 100, of how much the job details match the resume data from the candidate")
     reasons: str = Field(description="Reasons for the evaluation")
 
-extractor = LlamaExtract(api_key=ENV["llamacloud_api_key"])
-agent = extractor.get_agent(id="99d18493-f573-47ba-a421-d3336260d712")
 mcp = FastMCP(name = "Resume Matcher MCP")
 llm = Groq(model="qwen-qwq-32b", api_key=ENV["groq_api_key"])
 llm_struct = llm.as_structured_llm(JobMatchEvaluation)
 
-@mcp.tool(name="resume_parser", description="Parse a resume and extract important content from it. Requires the path_to_resume (str) argument as an input.")
-async def resume_parser(path_to_resume: str):
-    response = await agent.aextract(path_to_resume)
-    extracted_data = response.data
-    formatted_data = f"""
-    Potential Job Roles: {', '.join(extracted_data['potential_job_titles'])}
-    Seniority: {extracted_data['seniority']}
-    Skills: {', '.join(extracted_data['skills'])}
-    Based in: {extracted_data['based_in'] if extracted_data['based_in'] is not None else 'Information not available'}
-    Working location: {extracted_data['work_location'] if extracted_data['work_location'] is not None else 'Information not available'}
-    """
-    return formatted_data
+
 
 @mcp.tool(name="job_searcher", description="Search for a job with a given title and location. Requires: job_title (List[str]) - the job titles you are searching for, it's a list with one or more elements.")
 def job_searcher(job_title: List[str]):
@@ -94,3 +81,11 @@ async def evaluate_job_match(candidate_profile: str, jobs: str):
         json_response = json.loads(response.message.blocks[0].text)
         matches.update({f"{job['job_title']} at {job['company']} ({job['url']})": {"score": json_response['match_score'], "reasons": json_response['reasons']}})
     return json.dumps(matches)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--server_type", type=str, default="sse", choices=["sse", "stdio"]
+    )
+    args = parser.parse_args()
+    mcp.run(args.server_type)
